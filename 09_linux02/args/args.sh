429a5badcf21a767164b3414be6c78ee1e231472
Ben Halpern <benhalpern121@gmail.com>
2019-05-10 14:57:33 -0700

    fixed an error in 09

--- 09_linux02/args/args.sh
+++ 09_linux02/args/args.sh
@@ -6,14 +6,16 @@ echo "Hello World"
 echo $0
 
 # What does it contain ?
-# The argument $0 contains the current running command
-# $0 is the standard input, thus the last thing inputed into the terminal. In this case it was './args.sh'
+# The argument $0 contains the index value of 0 in the command line command being run:  bash thisscript.sh the $0 of this is bash or ./thisscript.sh the $0 is ./thisscript.sh
+
 
 # print out $1
 echo $1
 
 # what happens when you rub the script _with_ arguments???
+#it prints out the arguments run
 # What happens when you run the script _without_ arguments???
+#when run without arguments it prints out null
 
 # Write a script that acceprs _three_ arguments from the user, then Prints out the following:
 ####### The name of the script
